7f81e500dc12456f6d5bb9bc74ae8a1c8caf7bba
John Blischak <jdblischak@gmail.com>
2017-11-17 23:26:06 -0600

    Finish script to download FASTQ files.

--- code/fastq-download.py
+++ code/fastq-download.py
@@ -16,7 +16,42 @@ Options:
 """
 
 # Example usage:
-#   python code/fastq-download.py data/md5/YG-PYT-Fucci1.md5 Genomics/NGS-2017/171109_700819F_0583_ACAWRYACXX-YG-PYT-Fucci1 /project2/gilad/fucci-seq/fastq
+#   python code/fastq-download.py data/md5/YG-PYT-Fucci1.md5 Genomics/NGS-2017/171109_700819F_0583_ACAWRYACXX-YG-PYT-Fucci1/FastQ /project2/gilad/fucci-seq/fastq
+#
+# Implementation details:
+#
+# The md5 file has 2 columns, no header, and is space-delimted. The first column
+# is the md5 checksum. The second column is the file, though the path is not the
+# same as on the FTP server, and thus must be discarded. Also the file contains
+# the checksums for other miscellaneous files that need to be skipped. This
+# script only donwloads fastq.gz files, and also exlcudes the Undetermined
+# files. Here are two example lines from an input file:
+#
+# aed3c3f561e6e28fd16174620f13f127  /media/data1/NewSequencerRuns/171109_700819F_0583_ACAWRYACXX/Unaligned_YG-PYT-Fucci1/YG-PYT-Fucci1-20170905-B08_S20_L001_R1_001.fastq.gz
+# 214b05c57da97e00e0f4a2cf2ec47767  /media/data1/NewSequencerRuns/171109_700819F_0583_ACAWRYACXX/Unaligned_YG-PYT-Fucci1/YG-PYT-Fucci1-20170905-B09_S21_L001_R1_001.fastq.gz
+#
+# The downloaded files are further organized on the local filesystem. Each FASTQ
+# is put in a subdirectory corresponding to the C1 chip the single cell came
+# from. It will look something like this:
+#
+# outdir
+# ├── 20170905
+# │   ├── YG-PYT-Fucci1-20170905-A01_S1_L001_R1_001.fastq.gz
+# │   ├── YG-PYT-Fucci1-20170905-A02_S2_L001_R1_001.fastq.gz
+# │   ├── YG-PYT-Fucci1-20170905-A03_S3_L001_R1_001.fastq.gz
+# │   ├── YG-PYT-Fucci1-20170905-A04_S4_L001_R1_001.fastq.gz
+# │   ├── YG-PYT-Fucci1-20170905-A05_S5_L001_R1_001.fastq.gz
+# │   ├── YG-PYT-Fucci1-20170905-A06_S6_L001_R1_001.fastq.gz
+# │   ├── YG-PYT-Fucci1-20170905-A07_S7_L001_R1_001.fastq.gz
+# │   ├── YG-PYT-Fucci1-20170905-A08_S8_L001_R1_001.fastq.gz
+# │   └── YG-PYT-Fucci1-20170905-A09_S9_L001_R1_001.fastq.gz
+# ├── 20170906
+# ├── 20170907
+# ├── 20170908
+# └── 20170910
+#
+# The script will skip files that have already been downloaded and removes those
+# whose md5 checksum does not match.
 
 import docopt
 import getpass
@@ -25,7 +60,8 @@ import os
 import pysftp
 import sys
 
-def main(md5file, remotedir, outdir = ".", hostname = "fgfftp.uchicago.edu", username = "gilad"):
+def main(md5file, remotedir, outdir = ".", hostname = "fgfftp.uchicago.edu",
+         username = "gilad"):
 
     # Connect to server
     p = getpass.getpass("Authenticate: ")
@@ -34,9 +70,7 @@ def main(md5file, remotedir, outdir = ".", hostname = "fgfftp.uchicago.edu", use
     # Import md5 checksums computed by core
     md5_core = open(md5file, "r")
 
-    if not os.path.isdir(outdir):
-        os.mkdir(outdir)
-
+    # Download each file individually and verify the md5 checksum
     for line in md5_core:
         cols = line.strip().split()
         md5 = cols[0]
@@ -46,16 +80,19 @@ def main(md5file, remotedir, outdir = ".", hostname = "fgfftp.uchicago.edu", use
         sys.stdout.write("Downloading %s\n"%(fname))
         # Organize the FASTQ files into subdirectories based on the C1 chip
         chip = fname.split("-")[3]
-        outdir = outdir + "/" + chip
-        if not os.path.isdir(outdir):
-            os.mkdir(outdir)
-        localpath = outdir + "/" + fname
+        outdir_chip = outdir + "/" + chip
+        os.makedirs(outdir_chip, exist_ok = True)
+        localpath = outdir_chip + "/" + fname
         remotepath = remotedir + "/" + fname
-        if sftp.exists(remotepath):
-            sftp.get(remotepath, localpath)
-        else:
+        #import ipdb; ipdb.set_trace()
+        if not sftp.exists(remotepath):
             sys.stderr.write("Does not exist on remote server:\t%s\n"%(remotepath))
             continue
+        elif os.path.exists(localpath):
+            sys.stderr.write("Already exists:\t%s\n"%(remotepath))
+            continue
+        else:
+            sftp.get(remotepath, localpath)
         if not os.path.exists(localpath):
             sys.stderr.write("Download failed:\t%s\n"%(remotepath))
             continue
@@ -63,13 +100,13 @@ def main(md5file, remotedir, outdir = ".", hostname = "fgfftp.uchicago.edu", use
             md5_local = hashlib.md5(fq.read()).hexdigest()
             if md5_local != md5:
                 sys.stderr.write("Download incomplete:\t%s\n"%(remotepath))
+                os.remove(localpath)
 
-        md5_core.close()
-        sftp.close()
+    md5_core.close()
+    sftp.close()
 
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
-    print(args)
     main(md5file = args["<md5file>"],
          remotedir = args["<remotedir>"],
          outdir = args["<outdir>"])
