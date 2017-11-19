f6d88cca9e99d27efdee37be37f9023c08d8bb50
John Blischak <jdblischak@gmail.com>
2017-11-19 14:42:26 -0600

    Add scripts and data to incorporate lab information.

---
+++ code/extract-lab-info.R
@@ -0,0 +1,19 @@
+library("dplyr")
+x <- read.delim("../data/fucci_qc.txt", stringsAsFactors = FALSE)
+
+stopifnot(x$ERCC == "1:50000")
+
+out <- x %>%
+  mutate(sample = paste(experiment, well, sep = "-"),
+         ERCC = "50x dilution") %>%
+  select(sample, experiment:individual.2) %>%
+  arrange(sample)
+
+dir.create("../data/lab-info", showWarnings = FALSE)
+
+for (e in unique(out$experiment)) {
+  print(e)
+  fname <- paste0("../data/lab-info/", e, ".txt")
+  d <- out %>% filter(experiment == e)
+  write.table(d, file = fname, quote = FALSE, sep = "\t", row.names = FALSE)
+}
