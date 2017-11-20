14866ec93ddaba400f5c3e11809d0736d769a8cb
John Blischak <jdblischak@gmail.com>
2017-11-19 22:05:45 -0600

    Add combined counts and annotation files.

---
+++ code/output-exp-mat.R
@@ -0,0 +1,33 @@
+#!/usr/bin/env Rscript
+
+# Utility script to export counts for human and ERCC genes to gzipped
+# tab-delimited file.
+#
+# Usage:
+#
+#   Rscript output-exp-mat.R <directory with ExpressionSet rds files>
+#                            <output gzipped tab-delimited file>
+#
+
+suppressPackageStartupMessages(library("Biobase"))
+
+args <- commandArgs(trailingOnly = TRUE)
+stopifnot(length(args) == 2)
+dir_eset <- args[1]
+outfile <- args[2]
+
+fname <- Sys.glob(file.path(dir_eset, "*.rds"))
+eset <- Reduce(combine, Map(readRDS, fname))
+
+# Add individual to column names
+mat <- exprs(eset)
+colnames(mat) <- paste(eset$chip_id, eset$experiment, eset$well, sep = ".")
+
+# Make rownames of gene IDs a separate column to make it easier to read with
+# data.table::fread.
+mat_df <- data.frame(gene = rownames(mat), mat)
+
+gz_outfile <- gzfile(outfile, "w")
+write.table(mat_df, file = gz_outfile, quote = FALSE, sep = "\t",
+            row.names = FALSE)
+close(gz_outfile)
