92993647a97077322922dba771dd5c53ec83c50a
John Blischak <jdblischak@gmail.com>
2017-11-20 21:49:54 -0600

    Add lab info for chips on 2nd flow cell and add image metadata.

--- code/extract-lab-info.R
+++ code/extract-lab-info.R
@@ -1,12 +1,15 @@
 library("dplyr")
-x <- read.delim("../data/fucci_qc.txt", stringsAsFactors = FALSE)
+x <- read.delim("../data/fucci_qc.txt", stringsAsFactors = FALSE,
+                colClasses = c("character", "character", "numeric",
+                               "numeric", "character", "character",
+                               "character", "character", "character"))
 
 stopifnot(x$ERCC == "1:50000")
 
 out <- x %>%
   mutate(sample = paste(experiment, well, sep = "-"),
          ERCC = "50x dilution") %>%
-  select(sample, experiment:individual.2) %>%
+  select(sample, experiment:image_label) %>%
   arrange(sample)
 
 dir.create("../data/lab-info", showWarnings = FALSE)
