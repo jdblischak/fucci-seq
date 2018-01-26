85da8dd2b7c932903c8be0b6d0a91a93dc83f558
Joyce Hsiao <joyce.hsiao1@gmail.com>
2018-01-25 18:42:39 -0600

    make cell cycle gene lists into rds

---
+++ code/prepare-cellcycle-genelist.R
@@ -0,0 +1,37 @@
+###### ---- Get Macosko 2015 genes from the human experiment
+# Macosko identified 544 cell cycle genes using human cells
+# the output below gives 558 ensembl gene IDs, because
+# in several cases, a HGNC symbol is mapped to multiple ensembl gene ID
+macosko <- xlsx::read.xlsx("data/cellcycle-genes-previous-studies/cellcyclegenes-macosko-2015.xlsx",
+                           sheetName = 4, header = TRUE)
+tmp <- vector("list", length(macosko))
+
+for (i in 1:length(phases)) {
+  macosko[[i]] <- as.character(macosko[[i]])
+  macosko[[i]] <- gsub(" ", "", macosko[[i]])
+  tmp[[i]] <- macosko[[i]][!is.na(macosko[[i]])]
+}
+
+tmp <- data.frame(hgnc = c(tmp[[1]], tmp[[2]], tmp[[3]], tmp[[4]], tmp[[5]]),
+                  phase = c( rep(names(macosko)[1], length(tmp[[1]])),
+                             rep(names(macosko)[2], length(tmp[[2]])),
+                             rep(names(macosko)[3], length(tmp[[3]])),
+                             rep(names(macosko)[4], length(tmp[[4]])),
+                             rep(names(macosko)[5], length(tmp[[5]])) ),
+                  stringsAsFactors = FALSE)
+
+library(biomaRt)
+ensembl <- useMart("ensembl",dataset="hsapiens_gene_ensembl")
+
+biomart <- getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"),
+                 filters = "hgnc_symbol",
+                 values = tmp$hgnc,
+                 mart = ensembl)
+
+anno <- merge(macosko_anno, biomart, by.x="hgnc", by.y="hgnc_symbol", all=TRUE)
+anno$hgnc <- as.character(anno$hgnc)
+anno$phase <- as.character(anno$phase)
+
+colnames(anno) <- c("hgnc", "phase", "ensembl")
+
+saveRDS(anno, file = "data/cellcycle-genes-previous-studies/rds/macosko-2017.rds")
