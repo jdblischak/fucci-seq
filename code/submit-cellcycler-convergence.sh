c1fadbea2c8c456c5f4f4fe652bc783fb6d1d423
Joyce Hsiao <joyce.hsiao1@gmail.com>
2018-02-09 16:36:14 -0600

    minor edits

--- code/submit-cellcycler-convergence.sh
+++ code/submit-cellcycler-convergence.sh
@@ -5,16 +5,16 @@ set -e
 #   bash submit-cellcycler-convergence.sh
 #
 
-# iter=({1..20})
+# round=({1..20})
 # seed=(2  3  5  7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71)
 
-iter=(1)
+round=(1)
 seed=(2)
 
-for i in ${iter[@]}; do
+for ii_round in ${round[@]}; do
 
   ii_seed=${seed[i]}
-  sbatch cellcycler-convergence.sbatch ${ii_seed} $i
+  sbatch cellcycler-convergence.sbatch ${ii_seed} ${ii_round}
 
 done
 
