a90f9eb349bc707fc8e314b05af00aed05725648
Joyce Hsiao <joyce.hsiao1@gmail.com>
2018-07-06 14:05:29 -0500

    aadd a wrapper for trendfilter for when applying to the entire gene expression dataset

--- peco/R/fit.trendfilter.generic.R
+++ peco/R/fit.trendfilter.generic.R
@@ -1,3 +1,50 @@
+#' @title Get cyclical genes
+#'
+#' @export
+# Y <- data_training$log2cpm.quant.nonvalid[,fold_indices[[1]]$train]
+# theta <- data_training$theta.nonvalid[fold_indices[[1]]$train]
+# ncores=15
+# Y <- Y[1:10,]
+get.cyclical <- function(Y, theta, polyorder=2, ncores) {
+  library(genlasso)
+  library(assertthat)
+  library(parallel)
+
+  G <- nrow(Y)
+  N <- ncol(Y)
+
+  if (!assert_that(all.equal(names(theta), colnames(Y)))) {
+    Y_ordered <- Y[,match(names(theta), colnames(Y))]
+    ord <- order(theta)
+    theta_ordered <- theta[ord]
+    Y_ordered <- Y_ordered[,ord]
+  } else {
+    ord <- order(theta)
+    theta_ordered <- theta[ord]
+    Y_ordered <- Y[,ord]
+  }
+
+  # for each gene, estimate the cyclical pattern of gene expression
+  # conditioned on the given cell times
+  fit <- mclapply(1:G, function(g) {
+    #    print(g)
+    y_g <- Y_ordered[g,]
+
+    fit_g <- fit.trendfilter.generic(yy=y_g, polyorder = polyorder)
+
+    return(fit_g$pve)
+  }, mc.cores = ncores)
+
+  out <- do.call(rbind, fit)
+  out <- as.data.frame(out)
+  colnames(out) <- "pve"
+  rownames(out) <- rownames(Y_ordered)
+
+  return(out)
+}
+
+
+
 #' @title fitting trendfilter
 #'
 #' @param yy log2cpm gene expression vector
@@ -19,19 +66,14 @@ fit.trendfilter.generic <- function(yy, pos.yy=c(1:length(yy)), polyorder=2) {
   yy.trend.pred <- predict(fit.trend, lambda=cv.trend$lambda.1se,
                            df=fit.trend$df[which.lambda])$fit
   trend.yy <- yy.trend.pred
-  pve <- 1-var(yy-trend.yy)/var(yy)
+  pve <- 1-var(yy-trend.yy[which(include)])/var(yy)
 
   # trend.mad.pred <- mad(yy.nonzero.rep[include]-yy.trend.pred[include], constant = 1)
   # trend.mad.constant <- mad(yy.nonzero.rep[include]-mean(yy.nonzero.rep[include]), constant = 1)
   # trend.mad.ratio <- trend.mad.pred/trend.mad.constant
 
-  return(list(trend.yy=trend.yy[which(include)]
-              #trend.pos=pos.rep,
-#              include=include
-              #trend.mad.ratio=trend.mad.ratio,
-              #lambda=cv.trend$lambda.1se,
-              #trend.pve=pve
-              ))
+  return(list(trend.yy=trend.yy[which(include)],
+              pve=pve))
 }
 
 
