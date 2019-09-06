90545b6aeda3dfe2608453aed3c3132ebfc1d3d0
John Blischak <jdblischak@gmail.com>
2019-09-06 15:58:46 -0500

    Create figure and caption.

--- code/hg38.R
+++ code/hg38.R
@@ -6,8 +6,11 @@
 # Script executed in ~/fucci-seq-hg38/
 
 library(Biobase)
+library(cowplot)
 library(data.table)
+library(ggplot2)
 library(stringr)
+theme_set(theme_cowplot())
 
 # Input ------------------------------------------------------------------------
 
@@ -137,3 +140,79 @@ sums_per_sample <- rowSums(mol_hg19_shared_mat) + rowSums(mol_hg38_shared_mat)
 plot(sums_per_sample, cor_sample)
 
 # Plot -------------------------------------------------------------------------
+
+(p_gene <- ggplot(data.frame(cor = corrs), aes(x = cor)) +
+   geom_histogram(binwidth = 0.025) +
+   labs(x = "Pearson correlation", y = "Number of genes",
+        title = "Gene-gene correlation"))
+(p_samp <- ggplot(data.frame(cor = cor_sample), aes(x = cor)) +
+    geom_histogram(binwidth = 0.025) +
+    labs(x = "Pearson correlation", y = "Number of cells",
+         title = "Sample-sample correlation"))
+(p_cdk1 <- ggplot(data.frame(hg19 = unlist(mol_hg19_shared[, cycling_ensg[1], with = FALSE]),
+                             hg38 = unlist(mol_hg38_shared[, cycling_ensg[1], with = FALSE])),
+  aes(x = hg19, y = hg38)) +
+    geom_point(shape = "circle open") +
+    geom_abline(intercept = 0, slope = 1, color = "red", linetype = "dashed") +
+    labs(x = "hg19 gene counts", y = "hg38 gene counts",
+         title = sprintf("%s (%s)", cycling[1], cycling_ensg[1]),
+         subtitle = sprintf("Pearson correlation: %.5f",
+                            cor(unlist(mol_hg19_shared[, cycling_ensg[1], with = FALSE]),
+                                       unlist(mol_hg38_shared[, cycling_ensg[1], with = FALSE])))))
+
+(p_ube2c <- ggplot(data.frame(hg19 = unlist(mol_hg19_shared[, cycling_ensg[2], with = FALSE]),
+                             hg38 = unlist(mol_hg38_shared[, cycling_ensg[2], with = FALSE])),
+                  aes(x = hg19, y = hg38)) +
+    geom_point(shape = "circle open") +
+    geom_abline(intercept = 0, slope = 1, color = "red", linetype = "dashed") +
+    labs(x = "hg19 gene counts", y = "hg38 gene counts",
+         title = sprintf("%s (%s)", cycling[2], cycling_ensg[2]),
+         subtitle = sprintf("Pearson correlation: %.5f",
+                            cor(unlist(mol_hg19_shared[, cycling_ensg[2], with = FALSE]),
+                                unlist(mol_hg38_shared[, cycling_ensg[2], with = FALSE])))))
+
+(p_top2a <- ggplot(data.frame(hg19 = unlist(mol_hg19_shared[, cycling_ensg[3], with = FALSE]),
+                              hg38 = unlist(mol_hg38_shared[, cycling_ensg[3], with = FALSE])),
+                   aes(x = hg19, y = hg38)) +
+    geom_point(shape = "circle open") +
+    geom_abline(intercept = 0, slope = 1, color = "red", linetype = "dashed") +
+    labs(x = "hg19 gene counts", y = "hg38 gene counts",
+         title = sprintf("%s (%s)", cycling[3], cycling_ensg[3]),
+         subtitle = sprintf("Pearson correlation: %.5f",
+                            cor(unlist(mol_hg19_shared[, cycling_ensg[3], with = FALSE]),
+                                unlist(mol_hg38_shared[, cycling_ensg[3], with = FALSE])))))
+
+(p_hist1h4e <- ggplot(data.frame(hg19 = unlist(mol_hg19_shared[, cycling_ensg[4], with = FALSE]),
+                              hg38 = unlist(mol_hg38_shared[, cycling_ensg[4], with = FALSE])),
+                   aes(x = hg19, y = hg38)) +
+    geom_point(shape = "circle open") +
+    geom_abline(intercept = 0, slope = 1, color = "red", linetype = "dashed") +
+    labs(x = "hg19 gene counts", y = "hg38 gene counts",
+         title = sprintf("%s (%s)", cycling[4], cycling_ensg[4]),
+         subtitle = sprintf("Pearson correlation: %.5f",
+                            cor(unlist(mol_hg19_shared[, cycling_ensg[4], with = FALSE]),
+                                unlist(mol_hg38_shared[, cycling_ensg[4], with = FALSE])))))
+
+plot_grid(p_gene, p_samp, p_cdk1, p_ube2c, p_top2a, p_hist1h4e,
+          nrow = 3, labels = "AUTO")
+ggsave(filename = "hg38.png", width = 8, height = 12)
+
+# (A) The Pearson correlation for each gene comparing its counts across single
+# cells when mapping to genome build hg19 versus hg38. The median correlation
+# was 0.983919, and 1,070 genes (9.8%) had a correlation less than 0.9. (B) The
+# Pearson correlation for each sample comparing its gene counts when mapped to
+# genome build hg19 versus hg38.  The median correlation was 0.9849, and 306
+# single cells (19.9%) had a correlation less than 0.9. (C) A scatterplot of
+# cell cycle gene \textit{CDK1} (ENSG00000131747) gene counts for hg19 (x-axis)
+# versus hg38 (y-axis). The Pearson correlation across single cells was 0.99542.
+# (D) A scatterplot of cell cycle gene \textit{UBE2C} (ENSG00000170312) gene
+# counts for hg19 (x-axis) versus hg38 (y-axis). The Pearson correlation across
+# single cells was 0.99814. (E) A scatterplot of cell cycle gene \textit{TOP2A}
+# (ENSG00000175063) gene counts for hg19 (x-axis) versus hg38 (y-axis). The
+# Pearson correlation across single cells was 0.99948. (F) A scatterplot of cell
+# cycle gene \textit{HIST1H4E} (ENSG00000197061) gene counts for hg19 (x-axis)
+# versus hg38 (y-axis). The Pearson correlation across single cells was 0.99998.
+# The dashed red line represents the 1-1 line. All plots used data for the
+# 10,297 protein-coding genes that were shared between genome builds hg19 and
+# hg38. Note that cell cycle gene \textit{HIST1H4C} (ENSG00000198518) was
+# deprecated in the annotation for genome build hg38.
